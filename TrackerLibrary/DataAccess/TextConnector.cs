78a8356b3f2df4ea6dbcba4d9e7f25324ddd0f7d
Eddspace <Eduardo.Neves.Sousa@outlook.com>
2025-11-19 16:47:59 +0000

    Text Connection

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -1,14 +1,31 @@
 ﻿using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess;
 
 public class TextConnector : IDataConnection
 {
+    private const string PrizesFile = "PrizeModels.csv";
 
-    // TODO - Make the CreatePrize method actually save to text files
     public PrizeModel CreatePrize(PrizeModel model)
     {
-        model.Id = 1;
+        //load txt file & convert to list
+        List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+        //find last ID
+        int currentId = 1;
+        if (prizes.Count > 0)
+        {
+            currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+        }
+        model.Id = currentId;
+
+        //add new record with new id
+        prizes.Add(model);
+
+        //convert all to a list & replace txt with new list
+        prizes.SaveToPrizeFile(PrizesFile);
+
         return model;
     }
 }
